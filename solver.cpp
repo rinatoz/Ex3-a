631b30783c4cefe927242dc6a008801efe1cfb74
rinatoz <57804004+rinatoz@users.noreply.github.com>
2020-04-27 21:19:55 +0300

    Add files via upload

--- solver.cpp
+++ solver.cpp
@@ -1,157 +1,156 @@
 #include "solver.hpp"
 using namespace solver;
-
-RealVariable solver::operator+(RealVariable r, double n)
+RealVariable operator+(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator+(double n, RealVariable r)
+RealVariable operator+(double n, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator+(RealVariable l, RealVariable r)
+RealVariable operator+(RealVariable l, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator*(RealVariable r, double n)
+RealVariable operator*(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator*(double n, RealVariable r)
+RealVariable operator*(double n, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator*(RealVariable l, RealVariable r)
+RealVariable operator*(RealVariable l, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator-(RealVariable r, double n)
+RealVariable operator-(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator-(double n, RealVariable r)
+RealVariable operator-(double n, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator-(RealVariable l, RealVariable r)
+RealVariable operator-(RealVariable l, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator==(RealVariable r, double n)
+RealVariable operator==(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator==(double n, RealVariable r)
+RealVariable operator==(double n, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator==(RealVariable l, RealVariable r)
+RealVariable operator==(RealVariable l, RealVariable r)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator^(RealVariable r, double n)
+RealVariable operator^(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-RealVariable solver::operator/(RealVariable r, double n)
+RealVariable operator/(RealVariable r, double n)
 {
 	return RealVariable();
 }
 
-ComplexVariable solver::operator+(ComplexVariable r, double n)
+ComplexVariable operator+(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator+(double n, ComplexVariable r)
+ComplexVariable operator+(complex<double> n, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator+(ComplexVariable l, ComplexVariable r)
+ComplexVariable operator+(ComplexVariable l, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator*(ComplexVariable r, double n)
+ComplexVariable operator*(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator*(double n, ComplexVariable r)
+ComplexVariable operator*(complex<double> n, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator*(ComplexVariable l, ComplexVariable r)
+ComplexVariable operator*(ComplexVariable l, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator-(ComplexVariable r, double n)
+ComplexVariable operator-(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator-(double n, ComplexVariable r)
+ComplexVariable operator-(complex<double> n, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator-(ComplexVariable l, ComplexVariable r)
+ComplexVariable operator-(ComplexVariable l, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator==(ComplexVariable r, double n)
+ComplexVariable operator==(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator==(double n, ComplexVariable r)
+ComplexVariable operator==(complex<double> n, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator==(ComplexVariable l, ComplexVariable r)
+ComplexVariable operator==(ComplexVariable l, ComplexVariable r)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator^(ComplexVariable r, double n)
+ComplexVariable operator^(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-ComplexVariable solver::operator/(ComplexVariable r, double n)
+ComplexVariable operator/(ComplexVariable r, complex<double> n)
 {
 	return ComplexVariable();
 }
 
-double solver::solve(RealVariable ex)
+double solve(RealVariable ex)
 {
 	return 0.0;
 }
 
-double solver::solve(bool ex)
+double solve(bool ex)
 {
 	return 0.0;
 }
 
-complex<double> solver::solve(ComplexVariable ex)
+complex<double> solve(ComplexVariable ex)
 {
 	return complex<double>();
 }
--- solver.hpp
+++ solver.hpp
@@ -7,12 +7,14 @@ namespace solver
 {
 	class RealVariable
 	{
+
 	private:
 		double a;
 		double b;
 		double c;
 
 	public:
+
 		RealVariable() { this->a = 0; this->b = 0; this->c = 0; }
 		RealVariable(double a1, double b1, double c1) { this->a = a1; this->b = b1; this->c = c1; }
 
@@ -47,28 +49,31 @@ namespace solver
 		ComplexVariable() { this->a = 0; this->b = 0; this->c = 0; }
 		ComplexVariable(complex<double> a, complex<double> b, complex<double> c) { this->a = a; this->b = b; this->c; }
 
-		friend ComplexVariable operator+(ComplexVariable r, double n);
-		friend ComplexVariable operator+(double n, ComplexVariable r);
+		friend ComplexVariable operator+(ComplexVariable r, complex<double> n);
+		friend ComplexVariable operator+(complex<double> n, ComplexVariable r);
 		friend ComplexVariable operator+(ComplexVariable l, ComplexVariable r);
 
-		friend ComplexVariable operator*(ComplexVariable r, double n);
-		friend ComplexVariable operator*(double n, ComplexVariable r);
+		friend ComplexVariable operator*(ComplexVariable r, complex<double> n);
+		friend ComplexVariable operator*(complex<double> n, ComplexVariable r);
 		friend ComplexVariable operator*(ComplexVariable l, ComplexVariable r);
 
-		friend ComplexVariable operator-(ComplexVariable r, double n);
-		friend ComplexVariable operator-(double n, ComplexVariable r);
+		friend ComplexVariable operator-(ComplexVariable r, complex<double> n);
+		friend ComplexVariable operator-(complex<double> n, ComplexVariable r);
 		friend ComplexVariable operator-(ComplexVariable l, ComplexVariable r);
 
-		friend ComplexVariable operator==(ComplexVariable r, double n);
-		friend ComplexVariable operator==(double n, ComplexVariable r);
+		friend ComplexVariable operator==(ComplexVariable r, complex<double> n);
+		friend ComplexVariable operator==(complex<double> n, ComplexVariable r);
 		friend ComplexVariable operator==(ComplexVariable l, ComplexVariable r);
 
-		friend ComplexVariable operator^(ComplexVariable r, double n);
-		friend ComplexVariable operator/(ComplexVariable r, double n);
+		friend ComplexVariable operator^(ComplexVariable r, complex<double> n);
+		friend ComplexVariable operator/(ComplexVariable r, complex<double> n);
 	};
 
-		double solve(RealVariable ex);
-		double solve(bool ex);
-		complex<double> solve(ComplexVariable ex);
+
+	double solve(RealVariable ex);
+	double solve(bool ex);
+	complex<double> solve(ComplexVariable ex);
+
 
 }
+
